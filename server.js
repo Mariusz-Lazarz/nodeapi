5f65dedb63e3fb9d9469dd00a9f5489ced961b2b
Mariusz Łazarz <78878811+Mariusz-Lazarz@users.noreply.github.com>
2024-03-21 15:18:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,7 @@ const app = require("./app");
 const port = 3000;
 
 // connecting to mongoDB
-async function connectToDatabase() {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://iluu0456:test1234@cluster0.uqvw237.mongodb.net/nodeapi"
-    );
-    console.log(`DB is connected`);
-  } catch (error) {
-    console.error(`Error connecting to the database: ${error}`);
-  }
-}
+
 
 connectToDatabase();
 
